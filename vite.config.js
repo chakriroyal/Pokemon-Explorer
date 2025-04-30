0fefe1f04bf14fc9ee58bfc93943efc23547165d
chakriroyal <gantachakriroyal@gmail.com>
2025-04-30 21:45:25 +0530

    Build files for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Pokemon-Explorer/', // Use your GitHub repo name here
+  base: '/Pokemon-Explorer/', 
 })
